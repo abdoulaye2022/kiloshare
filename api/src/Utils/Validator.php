c9998251b8b5e29f80a3dd71087bef4ad89ccd44
Abdoulaye Mohamed Ahmed <m2atodev@gmail.com>
2025-10-04 20:06:13 -0300

    correction bug prod

--- api/src/Utils/Validator.php
+++ api/src/Utils/Validator.php
@@ -95,12 +95,14 @@ class Validator
 
     public static function date(): V
     {
-        return V::date();
+        // Valide que c'est une string au format date
+        return V::regex('/^\d{4}-\d{2}-\d{2}$/');
     }
 
     public static function datetime(): V
     {
-        return V::date();
+        // Valide que c'est une string au format date ou datetime
+        return V::regex('/^\d{4}-\d{2}-\d{2}(\s\d{2}:\d{2}:\d{2})?$/');
     }
 
     public static function in(array $values): V
